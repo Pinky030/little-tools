3003707cdf4ed6e8906bc473995b694566187ed0
Pinky Hui <dorogoy5288940@gmail.com>
2024-12-15 18:21:43 +0800

    chore: test route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
     <main>
       <Stack h={300} align="stretch" justify="center" gap="md">
         <Text size="lg" fw={700} c="orange" ta="center">
-          個人用小工具 ʕ•͡ᴥ•ʔ
+          個人用小工具 ʕ•͡ᴥ•ʔ 2
         </Text>
         <Image radius="md" h={200} w="auto" fit="contain" src={hachiware} />
       </Stack>
